7769989d0e14a35d816aff35b207032722ef1081
[wkx.ian] <[ipalmieri1208@gmail.com]>
2024-07-22 19:32:58 -0400

    Fisrt commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,15 @@
+
+
+
+export {crearDeck} from './crear-deck';
+export {pedirCarta} from './pedir-carta';
+export {turnoComputadora} from './turno-computadora';
+export {valorCarta} from './valor-carta';
+
+// ! aca estamos importando el creardDeck de forma indepediente 
+
+// todo: tambien podemos renombrar mediante un alias alguna funcion importada asi:
+// TODO USANDO EL AS QUE ES ALIAS
+// import {crearDeck as CREADNDONUEVODECK} from './usecases/crear-deck';
+
+//! tambien se puede importar de forma por defecto 
