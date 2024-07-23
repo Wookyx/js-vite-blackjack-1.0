20cc38797d162b25b2e714dd464c57f17f9e57e0
[wkx.ian] <[ipalmieri1208@gmail.com]>
2024-07-22 21:15:34 -0400

    update docs2.0

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// archivo de como configurar vite
+
+import { defineConfig } from "vite";// archivo de java script que espera un export default
+
+
+
+export default defineConfig({
+
+    // funcion que espera un objeto que tiene muchas funciones añadir plugins , cambiar directorios
+
+
+    base : '/js-vite-blackjack-1.0/',
+    
+
+});
+
+
+
